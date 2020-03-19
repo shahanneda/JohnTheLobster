44c7f803823da3817d8de2cd342633bf07d1eb43
Shahan Neda <shahan.neda@gmail.com>
2020-03-19 13:15:21 -0700

    initial commit

---
+++ Assets/FloorPass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorPass : MonoBehaviour
+{
+
+    public GameObject levelPrefab;
+    public Transform levelSpawnLocation;
+    public Transform levelParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if( col.gameObject.CompareTag("Player")){
+            print("I GOT HERE BUDDY");
+
+            GameObject newLevelPiece = Instantiate(levelPrefab, levelSpawnLocation.position, Quaternion.identity, levelParent) as GameObject;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float lastUpdateTime = Time.time;
+
+    public float timeBetweenUpdate = 1f;
+    public float moveSpeed = 2;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>(); 
+    }
+
+    void Update()
+    {
+        if(Time.time > lastUpdateTime + timeBetweenUpdate){
+            lastUpdateTime = Time.time;
+            rb.AddForce(new Vector2(+0.5f  *moveSpeed, -0.8f  *moveSpeed), ForceMode2D.Impulse);
+        }
+    }
+}
