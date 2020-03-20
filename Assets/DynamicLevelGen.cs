5d383b1d0e01c09d3188dfad5f9759dcf15d2ca3
Shahan Neda <shahan.neda@gmail.com>
2020-03-19 18:27:27 -0700

    got perlin noise working

--- Assets/DynamicLevelGen.cs
+++ Assets/DynamicLevelGen.cs
@@ -6,9 +6,22 @@ public class DynamicLevelGen : MonoBehaviour
 {
     public GameObject piece;
 
+    public float scale = 10;
+
     private void Start()
     {
+        Transform start = piece.transform.GetChild(0);
+        Transform end = piece.transform.GetChild(1);
+        float distance = Mathf.Abs(start.position.magnitude - end.position.magnitude);
 
 
+
+        for (int i = 0; i < 100; i++){
+            float test = Random.Range(-10, 10);
+            float noise = Mathf.PerlinNoise(i*0.01f, 1);
+            print(noise);
+
+            GameObject level = Instantiate(piece, new Vector3(transform.position.x + i*distance, transform.position.y + noise*scale, transform.position.z), Quaternion.identity, transform);
+        }
     }
 }
---
+++ Assets/PieceEnd.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceEnd : MonoBehaviour
+{
+
+    public GameObject piece;
+    public float scale = 10;
+    public Transform parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        Transform start = piece.transform.GetChild(0);
+        Transform end = piece.transform.GetChild(1);
+        float distance = Mathf.Abs(start.position.magnitude - end.position.magnitude);
+
+        float noise = Mathf.PerlinNoise(transform.position.x * 0.01f, 1);
+        print(noise);
+
+        GameObject level = Instantiate(piece, new Vector3(transform.position.x * distance + 100,
+                                                          transform.position.y + noise * scale, transform.position.z),
+                                       Quaternion.identity, parent);
+
+    }
+}
